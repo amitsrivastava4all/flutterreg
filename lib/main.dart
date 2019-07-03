9f41854ca581e1030abaf1d9d05e8f652b039a4b
Amit Srivastava <amit.srivastava4all@gmail.com>
2019-07-03 16:28:38 +0530

    Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,24 @@
 import 'package:flutter/material.dart';
 
-import './screens/gallery.dart';
+import './screens/servercall.dart';
 
+//https://github.com/rxlabz/audioplayer
 void main() {
   runApp(MaterialApp(
-      title: 'My First App',
-      home: Scaffold(
-        backgroundColor: Colors.yellow,
-        appBar: AppBar(
-          title: Text(
-            'First App -2019',
-            style: TextStyle(
-                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
-          ),
-          backgroundColor: Colors.lime,
+    title: 'Music Player',
+    home: Scaffold(
+      appBar: AppBar(
+        elevation: 6,
+        backgroundColor: Colors.red,
+        title: Text(
+          'Music Player...',
+          style: TextStyle(fontSize: 25),
         ),
-        body: Gallery(),
-//        body: Container(
-//            //margin: EdgeInsets.all(20),
-//            margin: EdgeInsets.only(left: 100),
-//            padding: EdgeInsets.all(30),
-//            height: 300,
-//            width: 300,
-//            color: Colors.red,
-//            child: Text(
-//              'Hello Container',
-//              style: TextStyle(
-//                  fontSize: 30,
-//                  fontWeight: FontWeight.bold,
-//                  color: Colors.white),
-//            )),
-      )));
+      ),
+      body: Container(child: ServerCall()
+          // child: Player(),
+          // child: FullPlayer(),
+          ),
+    ),
+  ));
 }
